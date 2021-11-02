5b55a32db64789d92a3ec171df4fccf000d19346
abunaserkayes0 <abunaserkayes0.gamil.com>
2021-11-02 20:09:40 +0600

    massage

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAJghS12uTVm7lrEgCIm42esEL_-RIhVp4",
+    authDomain: "burj-al-arab-dff95.firebaseapp.com",
+    projectId: "burj-al-arab-dff95",
+    storageBucket: "burj-al-arab-dff95.appspot.com",
+    messagingSenderId: "841663165413",
+    appId: "1:841663165413:web:98c8ed60654007b934b50c"
+};
+  
+export default firebaseConfig;
